6940025d83f5b79615353f3319464dbd3309b4eb
Jose Serrano Quesada <joseserranoq00@gmail.com>
2022-10-15 09:26:39 -0600

    .

---
+++ Proyecto_lenguajes2/src/main.java
@@ -0,0 +1,34 @@
+import java.util.Map;
+import org.jpl7.*;
+//import org.jpl7.Query;
+//import org.jpl7.Term;
+
+public class main {
+    public static void main(String[] args) {
+        tile[][] m ={{},{},{},{}};
+        
+        /*
+        String path = "consult('file.pl')";
+        Query q1 = new Query(path);
+        System.out.println(q1.hasSolution());
+        Variable X = new Variable("X");
+        Query q4 = 
+          new Query( 
+              "descendent_of", 
+              new Term[] {X,new Atom("ralf")} 
+          );
+
+        java.util.Map<String,Term> solution;
+
+        solution = q4.oneSolution();
+
+        System.out.println( "first solution of descendent_of(X, ralf)"); 
+        System.out.println( "X = " + solution.get("X"));        
+        String t1 = "tiene(X,gas)";
+        Query q2= new Query(t1);
+        Map x = q2.oneSolution();
+        System.out.println(x.get("X"));
+        */
+        
+    }
+}
---
+++ Proyecto_lenguajes2/src/tile.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jose_
+ */
+public class tile { //The tile of the map
+    boolean available;
+}
